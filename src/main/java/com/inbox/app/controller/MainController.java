c2fc95e222ee0e867c9c2984a23cecd1734801bb
michealmboni <mbonimichael@yahoo.fr>
2020-02-26 15:31:54 +0100

    login page

---
+++ src/main/java/com/inbox/app/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.inbox.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String showIndex() {
+		return "login";
+	}
+}
