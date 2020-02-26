669578559c15d086db816cbcbb6e4158f4f7fa0e
michealmboni <mbonimichael@yahoo.fr>
2020-02-26 10:47:18 +0100

    Projekt init

---
+++ src/main/java/com/inbox/app/InboxChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.inbox.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxChatAppApplication.class, args);
+	}
+
+}
